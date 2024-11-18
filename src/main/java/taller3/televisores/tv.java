93a0db82f4bfadbe1539fc5b9b2e0caf7af19231
meicon456 <jocastrore@unal.edu.co>
2024-11-17 19:03:19 -0500

    cree tv marca y control

---
+++ src/main/java/taller3/televisores/tv.java
@@ -0,0 +1,107 @@
+public class TV {
+    private static int numTV = 0;
+
+    private Marca marca;
+    private boolean estado;
+    private int canal;
+    private int volumen;
+    private int precio;
+    private Control control;
+
+    public TV(Marca marca, boolean estado) {
+        this.marca = marca;
+        this.estado = estado;
+        this.canal = 1;
+        this.volumen = 1;
+        this.precio = 500;
+        numTV++;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        if (canal >= 1 && canal <= 120 && estado) {
+            this.canal = canal;
+        }
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(int volumen) {
+        if (volumen >= 0 && volumen <= 7 && estado) {
+            this.volumen = volumen;
+        }
+    }
+
+    public Control getControl() {
+        return control;
+    }
+
+    public void setControl(Control control) {
+        this.control = control;
+    }
+
+    public static int getNumTV() {
+        return numTV;
+    }
+
+    public static void setNumTV(int num) {
+        numTV = num;
+    }
+
+    public void turnOn() {
+        estado = true;
+    }
+
+    public void turnOff() {
+        estado = false;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public void canalUp() {
+        if (estado && canal < 120) {
+            canal++;
+        }
+    }
+
+    public void canalDown() {
+        if (estado && canal > 1) {
+            canal--;
+        }
+    }
+
+    public void volumenUp() {
+        if (estado && volumen < 7) {
+            volumen++;
+        }
+    }
+
+    public void volumenDown() {
+        if (estado && volumen > 0) {
+            volumen--;
+        }
+    }
+}
